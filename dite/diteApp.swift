40a45ba01966da9007099780fe52070fcd406bc1
najla <alabdullahnajla3@gmail.com>
2024-05-01 15:11:54 -0700

    Initial Commit

---
+++ dite/diteApp.swift
@@ -0,0 +1,17 @@
+//
+//  diteApp.swift
+//  dite
+//
+//  Created by mac on 22/10/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct diteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
